e1120e315981657c68a265bccff049d39b0a6a01
Tomás Chaves <tomcchaves@gmail.com>
2023-02-03 22:03:33 -0300

    Requisito 7.

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,23 +1,72 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
+
     return (
       <table>
-        <tr>
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método de pagamento</th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
-          <th>Moeda de conversão</th>
-          <th>Editar/Excluir</th>
-        </tr>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            expenses.map((expense) => (
+              <tr key={ expense.id }>
+                <td>{ expense.description }</td>
+                <td>{ expense.tag }</td>
+                <td>{ expense.method }</td>
+                <td>{ parseFloat(expense.value).toFixed(2) }</td>
+                <td>
+                  {
+                    Object.values(expense.exchangeRates)
+                      .find((element) => element.code === expense.currency).name
+                  }
+                </td>
+                <td>
+                  {
+                    Number((Object.values(expense.exchangeRates)
+                      .find((element) => element.code === expense.currency).ask))
+                      .toFixed(2)
+                  }
+                </td>
+
+                <td>
+                  {
+                    ((Object.values(expense.exchangeRates)
+                      .find((element) => element.code === expense.currency)
+                      .ask) * expense.value)
+                      .toFixed(2)
+                  }
+                </td>
+                <td>Real</td>
+              </tr>
+            ))
+          }
+        </tbody>
       </table>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.object,
+}.isRequired;
+
+export default connect(mapStateToProps)(Table);
